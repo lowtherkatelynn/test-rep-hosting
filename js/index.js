e85a1a1bc2e36e26e3fe3f7292eb009427e26c39
Katelynn Lowther <39056972+lowtherkatelynn@users.noreply.github.com>
2018-05-22 09:52:57 -0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+let i = 0;
+
+let chorus = "Put the bass in your walk Head to toe let your whole body talk Cover Girl! Put your bass in your walk Head-to-toe let your whole body talk";
+
+let lineTwo = "To Grumman's Chinese, Red carpet tv Valet my limousine Box office sweetie, An Oscar nominee Now watch me snatch trophies";
+
+let lineOne = "Stroll down the runway Another payday Cover of magazines And when they see me They want to be me I am your fantasy";
+
+
+//for (i = 0; i < 2; i++) {
+//  alert(chorus)   
+//} if (i = 1) {
+//    alert(lineTwo)
+//    i++
+//}
+
+if (i = 0) {
+    alert(lineOne);
+    i++;
+} else if (i = 1) {
+    alert(chorus);
+    i++;
+} else if (i = 2) {
+    alert(lineTwo);
+    i++;
+} else {
+    alert(chorus);
+}
